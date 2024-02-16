1c14281ef7383841d3517ea539ce88e5d9890228
ngoduykha1992 <52314863+ngoduykha1992@users.noreply.github.com>
2024-02-16 14:59:02 +0700

    Update index.py

--- index.py
+++ index.py
@@ -54,7 +54,7 @@ def get_conversational_chain():
     Answer:
     """
 
-    model = ChatGoogleGenerativeAI(model="gemini-pro",
+    model = ChatGoogleGenerativeAI(model="gemini-1.0-pro",
                                    client=genai,
                                    temperature=0.3,
                                    )
